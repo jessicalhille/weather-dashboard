3bf4712323f360a65281c4f48350c814a8d4d216
Jessica Hille <jessicalhille@gmail.com>
2021-11-22 18:51:08 -0600

    began to add css,fixed layout in html

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-var apiKey = "6e420bce715508b6be7bea964633132d";
-
-var queryUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
-$("submit").on("click", function(event) {
-    event.preventDefault();
-
-    console.log(city);
-})

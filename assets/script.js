5bae6f643f77e57c68a53d287f2080988f5bbda7
Jessica Hille <jessicalhille@gmail.com>
2021-11-28 21:01:17 -0600

    current weather and five day forecast appear correctly

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ apiKey = "6e420bce715508b6be7bea964633132d";
 var searchHistory = [];
 var today = moment().format("L");
 
+var fiveDayEl = document.querySelector("#five-day");
+
 function currentCondition(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
@@ -42,7 +44,7 @@ function currentCondition(city) {
 
             $("#city-info").append(uvIndexP);
 
-            // futureDays(latitute, longitude);
+            fiveDays(latitude, longitude);
 
             if (uvIndex >= 0 && uvIndex <= 3.5) {
                 $("#uvIndexColor").css("background-color", "#008000");
@@ -54,6 +56,47 @@ function currentCondition(city) {
         })
 
     })
+}
+
+function fiveDays(latitude, longitude) {
+    var fiveDaysUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current,minutely,hourly,alerts&appid=" + apiKey;
+
+    $.ajax({
+        url: fiveDaysUrl,
+        method: "GET"
+    }).then(function(fiveDayResponse) {
+        console.log(fiveDayResponse);
+
+        $("#five-day").empty();
+
+        for (i = 1; i < 6; i++) {
+            var cityInfo = {
+                date: fiveDayResponse.daily[i].dt,
+                icon: fiveDayResponse.daily[i].weather[0].icon,
+                temp: fiveDayResponse.daily[i].temp.day,
+                wind: fiveDayResponse.daily[i].wind_speed,
+                humidity: fiveDayResponse.daily[i].humidity
+            };
+
+            var currentDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+
+            var fiveDayCards = $(`
+                <div class="pl-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>${currentDate}</h5>
+                            <p>Temp: ${cityInfo.temp} °F</p>
+                            <p>Wind: ${cityInfo.wind} MPH</p>
+                            <p>Humidity: ${cityInfo.humidity}\%</p>
+                        </div>
+                    </div>
+                </div>
+            `);
+
+            $("#five-day").append(fiveDayCards);
+        };
+
+    });
 }
 
 $("#searchBtn").on("click", function(event) {
